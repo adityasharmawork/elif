2fbb19f1d1089faa3923dbcbce8eb853a22ff2a0
Aditya Sharma <adityasharma2005123@gmail.com>
2025-02-23 13:02:32 +0530

    Fixed .env location issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      Home page
+      <SignedOut>
+          <SignInButton>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Sign In
+            </button>
+          </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Sign Out
+          </button>
+        </SignOutButton>
+      </SignedIn>
     </div>
   );
 }
