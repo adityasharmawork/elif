8224083c4a8a211f0cc3bb3e004b774e4d9ffded
Aditya Sharma <adityasharma2005123@gmail.com>
2025-02-27 22:32:44 +0530

    Zustand Store Updates

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useState } from "react";
 
 const ThemeSelector = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { theme, setTheme } = useCodeEditorStore();
   return (
     <div>ThemeSelector</div>
   )
--- src/store/useCodeEditorStore.ts
+++ src/store/useCodeEditorStore.ts
@@ -2,6 +2,7 @@ import { LANGUAGE_CONFIG } from "@/app/(root)/_constants";
 import { create } from "zustand";
 import { Editor, Monaco } from "@monaco-editor/react";
 import { CodeEditorState } from "@/types";
+import { currentUser } from "@clerk/nextjs/server";
 
 
 
@@ -43,6 +44,38 @@ export const useCodeEditorStore = create<CodeEditorState>((set, get) => {
         setEditor: (editor: Monaco) => {
             const savedCode = localStorage.getItem(`editor-code-${get().language}`);
             if (savedCode) editor.setValue(savedCode);
+
+            set({ editor });
+        },
+
+        setTheme: (theme: string) => {
+            localStorage.setItem("editor-theme", theme);
+            set({ theme });
+        },
+
+        setFontSize: (fontSize: number) => {
+            localStorage.setItem("editor-font-size", fontSize.toString());
+            set({ fontSize });
+        },
+
+        setLanguage: (language: string) => {
+            // Save current language code before switching
+            const currentCode = get().editor?.getValue();
+            if(currentCode) {
+                localStorage.setItem(`editor-code-${get().language}`, currentCode);
+            }
+
+            localStorage.setItem("editor-language", language);
+
+            set({
+                language,
+                output: "",
+                error: null,
+            });
+        },
+
+        runCode: async () => {
+            // todo
         }
     }
 })
