a6142466f31013fbc47e958574f0b572e1bd8493
Aditya Sharma <adityasharma2005123@gmail.com>
2025-03-01 23:37:21 +0530

    Changes in Editor Panel

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -2,15 +2,20 @@
 
 import { useCodeEditorStore } from "@/store/useCodeEditorStore";
 import { useEffect, useState } from "react"
-import { LANGUAGE_CONFIG } from "../_constants";
+import { defineMonacoThemes, LANGUAGE_CONFIG } from "../_constants";
 import { Editor } from "@monaco-editor/react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { RotateCcwIcon, ShareIcon, TypeIcon } from "lucide-react";
+import { useClerk } from "@clerk/nextjs";
+import { EditorPanelSkeleton, EditorViewSkeleton } from "./EditorPanelSkeleton";
+import useMounted from "@/hooks/useMounted";
 
 function EditorPanel () {
+  const clerk = useClerk();
   const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
   const { language, theme, fontSize, editor, setFontSize, setEditor } = useCodeEditorStore();
+  const mounted = useMounted();
 
   useEffect(() => {
     const savedCode = localStorage.getItem(`editor-code-${language}`);
@@ -36,6 +41,7 @@ function EditorPanel () {
   }
 
 
+  if(!mounted) return <EditorViewSkeleton />
 
   return (
     <div className="relative">
@@ -96,8 +102,39 @@ function EditorPanel () {
 
         {/* Editor */}
 
-        <div className="relative ">
+        <div className="relative group rounded-xl overflow-hidden ring-1 ring-white/[0.05]">
+          { clerk.loaded && (<Editor 
+            height="600px"
+            language={LANGUAGE_CONFIG[language].monacoLanguage}
+            onChange={handleEditorChange}
+            theme={theme}
+            beforeMount={defineMonacoThemes}
+            onMount={(editor) => setEditor(editor)}
 
+            options={{
+              minimap: {enabled: false},
+              fontSize,
+              automaticLayout: true,
+              scrollBeyondLastLine: false,
+              padding: {top: 16, bottom: 16},
+              renderWhiteSpace: "selection",
+              fontFamily: '"Fira Code", "Cascadia Code", Cansolas, monospace',
+              fontLigatures: true,
+              cursorBlinking: "smooth",
+              smoothScrolling: true,
+              contextmenu: true,
+              renderLineHighlight: "all",
+              lineHeight: 1.6,
+              letterSpacing: 0.5,
+              roundedSelection: true,
+              scrollbar: {
+                verticalScrollbarSize: 8,
+                horizontalScrollbarSize: 8,
+              },
+            }}
+          /> )}
+
+          {/* { !clerk.loaded && <EditorPanelSkeleton /> } */}
         </div>
 
       </div>
