314922c95493a07f29f33be3bc5335dee3dc6903
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-05 17:37:20 +0530

    Made Snippet Page Skeleton for loading state

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,7 +1,29 @@
-function page() {
-  return (
-    <div>snippets page</div>
-  )
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import SnippetsPageSkeleton from "./_components/SnippetsPageSkeleton";
+
+function SnippetsPage() {
+    const snippets = useQuery(api.snippets.getSnippets);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+    const [view, setView] = useState<"grid" | "list">("grid");
+
+    // Loading state
+    if(snippets === undefined) {
+        return (
+            <div className="min-h-screen">
+                <NavigationHeader />
+                <SnippetsPageSkeleton />
+            </div>
+        )
+    }
+
+    return (
+        <div></div>
+    );
 }
 
-export default page
+export default SnippetsPage;
