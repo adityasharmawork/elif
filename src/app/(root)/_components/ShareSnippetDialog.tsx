acd4ad7566bfc622640ed2ff521d0562b9fa36ad
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-05 14:33:14 +0530

    Added createSnippet

--- src/app/(root)/_components/ShareSnippetDialog.tsx
+++ src/app/(root)/_components/ShareSnippetDialog.tsx
@@ -1,10 +1,13 @@
 import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useMutation } from "convex/react";
 import { useState } from "react"
+import { api } from "../../../../convex/_generated/api";
 
 function ShareSnippetDialog({onClose}: {onClose: () => void}) {
     const [title, setTitle] = useState("");
     const [isSharing, setIsSharing] = useState(false);
     const { language, getCode } = useCodeEditorStore();
+    const createSnippet = useMutation(api.snippets.createSnippet);
 
   return (
     <div>ShareSnippetDialog</div>
