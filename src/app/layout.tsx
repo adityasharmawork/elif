f7ff13747238c5bd728057524a9c562db43e36d5
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-05 15:57:57 +0530

    Built Code Snippet Sharing Feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,8 @@ export default function RootLayout({
           </ConvexClientProvider>
 
           <Footer/>
+
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
