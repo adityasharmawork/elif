754d2be6b7306e0e8cd1752f80e09ecacd4d4553
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-18 14:50:43 +0530

    Added star functionality

--- src/components/StarButton.tsx
+++ src/components/StarButton.tsx
@@ -1,6 +1,20 @@
+import { useAuth } from "@clerk/nextjs"
 import { Id } from "../../convex/_generated/dataModel"
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 function StarButton({snippetId}: {snippetId: Id<"snippets">}) {
+    const { isSignedIn } = useAuth();
+    const isStarred = useQuery(api.snippets.isSnippetStarred, { snippetId });
+    const starCount = useQuery(api.snippets.getSnippetStarCount, { snippetId });
+    const star = useMutation(api.snippets.starSnippet);
+
+    const handleStar = async () => {
+        if(!isSignedIn) return;
+        await star({ snippetId });
+    }
+
+
   return (
     <div>
 
