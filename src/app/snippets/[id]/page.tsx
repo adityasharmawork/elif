e838475732f05288e2a709f93c81121de1266903
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-18 18:46:00 +0530

    Made Snippet Loading Skeleton page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,20 @@
+"use client";
 
-function page() {
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import SnippetLoadingSkeleton from "./_components/SnippetLoadingSkeleton";
+
+
+function SnippetDetailPage() {
+    const snippetId = useParams().id;
+
+    const snippet = useQuery(api.snippets.getSnippetById, { snippetId: snippetId as Id<"snippets"> });
+
+
+    if(snippet === undefined) return <SnippetLoadingSkeleton />
+
   return (
     <div>
         Snippet Detail Page
@@ -7,4 +22,4 @@ function page() {
   );
 }
 
-export default page;
+export default SnippetDetailPage;
