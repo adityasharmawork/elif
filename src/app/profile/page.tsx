1102f7547ce757199caba50ee325c2d54b5df6d3
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-21 23:20:11 +0530

    Profile page init

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { usePaginatedQuery, useQuery } from "convex/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { api } from "../../../convex/_generated/api";
 
 function ProfilePage() {
+    const { user, isLoaded } = useUser();
+    const router = useRouter();
+
+    const [activeTab, setActiveTab] = useState<"executions" | "starred">("executions");
+
+    const userStats = useQuery(api.codeExecutions.getUserStats, { userId: user?.id ?? "" });
+
+    const starredSnippets = useQuery(api.snippets.getStarredSnippets);
+
+    const { results: executions, status: executionStatus, isLoading: isLoadingExecutions, loadMore } = usePaginatedQuery(
+        api.codeExecutions.getUserExecutions, {
+            userId: user?.id ?? "" 
+        }, 
+        { 
+            initialNumItems: 5 
+        } 
+    );
+
+    const userData = useQuery(api.users.getUser, { userId: user?.id ?? "" });
+
   return (
     <div>ProfilePage</div>
   )
