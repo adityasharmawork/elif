febae91022320e84107768b56d7674b69b99bfdd
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-22 22:39:13 +0530

    Fetching data from backend for landing page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,8 +1,16 @@
 import { currentUser } from "@clerk/nextjs/server"
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "../../../convex/_generated/api";
 
 async function LandingPage() {
 
     const user = await currentUser();
+
+    const convex = new ConvexHttpClient(process.env.Next_PUBLIC_CONVEX_URL!);
+
+    const convexUser = await convex.query(api.users.getUser, {
+        userId: user?.id || ""
+    });
 
   return (
     <div>LandingPage</div>
