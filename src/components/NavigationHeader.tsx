c135a0ba8dfc33a5086532043c8a62eee0ef0c32
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-23 20:40:57 +0530

    Fixed headers

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,6 +1,6 @@
 import HeaderProfileBtn from "@/app/(root)/_components/HeaderProfileBtn";
 import { SignedOut } from "@clerk/nextjs";
-import { Blocks, Code2 } from "lucide-react";
+import { Blocks, Code2, Code } from "lucide-react";
 import Link from "next/link";
 
 function NavigationHeader() {
@@ -20,7 +20,7 @@ function NavigationHeader() {
 
               {/* Logo */}
               <div className="relative bg-gradient-to-br from-[#1a1a2e] to-[#0a0a0f] p-2 rounded-xl ring-1 ring-white/10 group-hover:ring-white/20 transition-all">
-                <Blocks className="w-6 h-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
+                <Code className="w-6 h-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
               </div>
 
               <div className="relative">
@@ -28,10 +28,10 @@ function NavigationHeader() {
                   className="block text-lg font-semibold bg-gradient-to-r
                  from-blue-400 via-blue-300 to-purple-400 text-transparent bg-clip-text"
                 >
-                  CodeCraft
+                  Elif
                 </span>
                 <span className="block text-xs text-blue-400/60 font-medium">
-                  Interactive Code Editor
+                  Code Beyond Limits!
                 </span>
               </div>
             </Link>
