b6c535169a75a5e7471a58405655817b5966342e
Aditya Sharma <adityasharma2005123@gmail.com>
2025-04-23 21:47:03 +0530

    Navigation header changes on Editor Panel

--- src/app/editor/_components/Header.tsx
+++ src/app/editor/_components/Header.tsx
@@ -41,7 +41,7 @@
 // import { ConvexHttpClient } from "convex/browser";
 // import { api } from "../../../../convex/_generated/api";
 import Link from "next/link";
-import { Blocks, Code2 } from "lucide-react";
+import { Code, Code2 } from "lucide-react";
 import { SignedIn } from "@clerk/nextjs";
 import ThemeSelector from "./ThemeSelector";
 // import LanguageSelector from "./LanguageSelector";
@@ -77,15 +77,15 @@ async function Header() {
               className="relative bg-gradient-to-br from-[#1a1a2e] to-[#0a0a0f] p-2 rounded-xl ring-1
               ring-white/10 group-hover:ring-white/20 transition-all"
             >
-              <Blocks className="size-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
+              <Code className="size-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
             </div>
 
             <div className="flex flex-col">
               <span className="block text-lg font-semibold bg-gradient-to-r from-blue-400 via-blue-300 to-purple-400 text-transparent bg-clip-text">
-                CodeCraft
+                Elif
               </span>
               <span className="block text-xs text-blue-400/60 font-medium">
-                Interactive Code Editor
+                Code Beyond Limits!
               </span>
             </div>
           </Link>
